fb0e604514e72a98a1b22957da40e5221739b3bb
MundiaNderi <sarahngima77@gmail.com>
2023-07-25 22:07:55 +0300

    Promises

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,9 @@
+export function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const response = { data: 'Sample API response' };
+        resolve(response);
+      }, 1000); 
+    });
+  }
+  
