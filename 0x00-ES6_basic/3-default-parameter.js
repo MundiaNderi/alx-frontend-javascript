0535a04eba142ae71ea601f0f03b4f949cce8793
MundiaNderi <sarahngima77@gmail.com>
2023-07-25 21:48:54 +0300

    Rest parameter syntax for functions

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -2,7 +2,6 @@ export default function getSumOfHoods(initialNumber="", expansion1989, expansion
     if (expansion1989 === undefined) {
       expansion1989 = 89;
     }
-  
     if (expansion2019 === undefined) {
       expansion2019 = 19;
     }
