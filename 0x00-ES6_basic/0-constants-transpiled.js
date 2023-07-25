e07ea9b103dc0f32282e07d5b7a1e3e2719776cc
MundiaNderi <sarahngima77@gmail.com>
2023-07-25 14:22:32 +0300

    const or let

--- 0x00-ES6_basic/0-constants-transpiled.js
+++ 0x00-ES6_basic/0-constants-transpiled.js
@@ -2,18 +2,14 @@
 // function taskNext to instantiate variables using let
 
 export function taskFirst() {
-  const task = 'I prefer const when I can.'; // Using const
-  var additionalTask = 'This is a var variable.';
-  return task + ' ' + additionalTask;
+  const task = 'I prefer const when I can.';
+  return task;
 }
-
 export function getLast() {
   return ' is okay';
 }
-
 export function taskNext() {
-  let combination = 'But sometimes let'; // Using let
+  let combination = 'But sometimes let';
   combination += getLast();
-
   return combination;
 }
