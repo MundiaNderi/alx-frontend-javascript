f79d86efd331ecc76e177ca1b638593f32037a79
MundiaNderi <sarahngima77@gmail.com>
2023-07-26 19:50:42 +0300

    Await Async

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,18 @@
+import createUser from  './utils.js';
+
+async function asyncUploadUser() {
+    try {
+      const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+      return {
+        photo,
+        user,
+      };
+    } catch (error) {
+      return {
+        photo: null,
+        user: null,
+      };
+    }
+  }
+  
+  export default asyncUploadUser;
