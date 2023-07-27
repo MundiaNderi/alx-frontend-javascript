cb031bef09a7880f268ef6f33d447db76d84e73c
MundiaNderi <sarahngima77@gmail.com>
2023-07-27 19:36:11 +0300

    Holberton class

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,17 @@
+/*eslint-disable */
+
+class HolbertonClass{
+    constructor(size, location){
+        this._size = size;
+        this._location = location;
+    }
+    get size() {
+        return this._size;
+    }
+
+    get location() {
+        return this._location;
+    }
+}
+
+export default HolbertonClass;
