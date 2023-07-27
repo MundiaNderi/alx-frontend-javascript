0529ff2ca2b2f41dda6846262675da2f5c7e0488
MundiaNderi <sarahngima77@gmail.com>
2023-07-27 12:45:27 +0300

    Adding display full currency

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,10 +1,3 @@
-//Implement a class named Currency:
-// Constructor attributes:
-// code (String)
-// name (String)
-// Each attribute must be stored in an “underscore” attribute version (ex: name is stored in _name)
-// Implement a getter and setter for each attribute.
-// Implement a method named displayFullCurrency that will return the attributes in the following format name (code).
 /*eslint-disable */
 class Currency {
     constructor(code, name) {
@@ -26,6 +19,10 @@ class Currency {
 
     set name(newName) {
         this._name = this.ValidatedString(name, "Name"); 
+    }
+
+    displayFullCurrency() {
+        return `${this._name} (${this._code})`;
     }
 
     validateString(value, attribute) {
