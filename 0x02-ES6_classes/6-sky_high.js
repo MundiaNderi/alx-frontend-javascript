1f4e4796d65902e10dc96b0002de2af4ef5468b2
MundiaNderi <sarahngima77@gmail.com>
2023-07-27 18:59:15 +0300

    Inheritance

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,20 @@
+/*eslint-disable */
+import Building from './5-building.js';
+
+
+class SkyHighBuilding extends Building {
+    constructor(sqft, floors) {
+        super(sqft);
+        this._floors = floors;
+    }
+
+    get floors() {
+        return this._sqft;
+    }
+
+    evacuationWarningMessage() {
+        return 'Evacuate slowly the ${this._floors}  floors';
+    }
+}
+
+export default SkyHighBuilding;
