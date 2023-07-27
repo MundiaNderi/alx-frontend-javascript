1d3b6eaee6da1374a5a0ae8827637565e7ca8c10
MundiaNderi <sarahngima77@gmail.com>
2023-07-27 20:45:24 +0300

    Evcar

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,19 @@
+/*eslint-disable */
+import Car from "./10-car.js";
+
+const _range = Symbol('range');
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this[_range] = range;
+  }
+
+  get range() {
+    return this[_range];
+  }
+
+  cloneCar() {
+    return new Car(this.brand, this.motor, this.color);
+  }
+}
