9249db2a73c3fa6d9e8959b4041810b06a791ca3
MundiaNderi <sarahngima77@gmail.com>
2023-07-28 20:32:14 +0300

    more objects

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,16 +1,12 @@
 /*eslint-disable */
-import getBudgetObject from './7-getBudgetObject.js';
+import getBudgetObject from './7-getBudgetObject';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars: function () {
-      return `$${this.income}`;
-    },
-    getIncomeInEuros: function () {
-      return `${this.income} euros`;
-    },
+    getIncomeInDollars: (income) => `$${income}`,
+    getIncomeInEuros: (income) => `${income} euros`,
   };
 
   return fullBudget;
