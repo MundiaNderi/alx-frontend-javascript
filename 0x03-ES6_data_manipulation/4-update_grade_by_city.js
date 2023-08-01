fed9aad49882070f86a4dbadf44e0e617143a507
MundiaNderi <sarahngima77@gmail.com>
2023-08-01 17:47:10 +0300

    Combine

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,10 +1,18 @@
 /*eslint-disable */
-export default function updateStudentGradeByCity(students, city, newGrades) {
+function updateStudentGradeByCity(students, city, newGrades) {
+  // First, create a map of studentId to grade for quick access
+  const gradeMap = newGrades.reduce((map, entry) => {
+    map[entry.studentId] = entry.grade;
+    return map;
+  }, {});
+
+  // Then, filter the students by the given city and update the grades using the map
   return students
-    .filter(student => student.location === city)
-    .map(student => {
-      const gradeObj = newGrades.find(grade => grade.studentId === studentId);
-      const grade = gradeObj ? gradeObj.grade : 'N/A';
-      return { ...student, grade };
-    });
+    .filter((student) => student.location === city)
+    .map((student) => ({
+      ...student,
+      grade: gradeMap[student.id] || 'N/A', // Use 'N/A' if no grade is found
+    }));
 }
+
+export default updateStudentGradeByCity;
